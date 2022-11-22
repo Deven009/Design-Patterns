60934591a595d86b4bd8342bc3c7460eca702aea
Deven-Prajapati-2 <dprajap9@asu.edu>
2022-11-22 04:12:24 -0700

    minor refactor

--- src/singleton/Data.java
+++ src/singleton/Data.java
@@ -2,10 +2,11 @@ package singleton;
 
 public class Data {
     private int dataValue;
-    private static Data instance = new Data();
+    private static Data instance;
     private Data() {}
 
     public static Data getInstance() {
+        if(instance == null) instance = new Data();
         return instance;
     }
 
